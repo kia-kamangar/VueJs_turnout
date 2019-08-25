b18a18fbb78223abb5f52ae5dc21378aec44bba5
kia <kia.kamangar@gmail.com>
2019-08-25 10:07:17 +0430

    Add signIn field and button and define new user in firebase and show error message

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="">
-    Sign In Component
+  <div class="form-inline">
+    <h3>Sign In</h3>
+    <div class="form-group">
+      <input type="text" placeholder="Email" class="form-control" v-model='email'>
+      <input type="password" placeholder="Password" class="form-control" v-model='password'>
+      <button class="btn btn-primary" @click="signIn">Sign In</button>
+    </div>
+    <br><br>
+    <p>{{error.message}}</p>
+    <br>
   </div>
 </template>
+
+<script>
+import { firebaseApp } from '../firebaseApp.js'
+  export default {
+    data() {
+      return {
+        email: '',
+        password: '',
+        error: {
+          message: ''
+        }
+      }
+    },
+    methods: {
+      signIn() {
+        firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+          .catch(error => {
+            this.error = error
+          })
+      }
+    }
+  }
+</script>
