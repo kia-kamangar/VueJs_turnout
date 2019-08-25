7e5d4b84c3b2db33e01010ad27d77f6b8251fb3f
kia <kia.kamangar@gmail.com>
2019-08-25 09:38:13 +0430

    Add firebaseApp file and create new project in them and set when user exist push to dashboard else replace to signin

---
+++ src/firebaseApp.js
@@ -0,0 +1,14 @@
+
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBX7NOHTWnkqxGruaK-txgw87Z0EWvGOIg",
+  authDomain: "turnout-fb07c.firebaseapp.com",
+  databaseURL: "https://turnout-fb07c.firebaseio.com",
+  projectId: "turnout-fb07c",
+  storageBucket: "",
+  messagingSenderId: "668081755053",
+  appId: "1:668081755053:web:c7919b9ed0f265a0"
+};
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig)
