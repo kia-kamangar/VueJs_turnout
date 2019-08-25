b02968037fc42730ed5fd867e27dc2456d03beb7
kia <kia.kamangar@gmail.com>
2019-08-25 13:00:48 +0430

    Add link to signin and signUp in them and add signup file and page

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="form-inline">
-    <h3>Sign In</h3>
+    <h3>Sign Up</h3>
     <div class="form-group">
       <input type="text" placeholder="Email" class="form-control" v-model='email'>
       <input type="password" placeholder="Password" class="form-control" v-model='password'>
-      <button class="btn btn-primary" @click="signIn">Sign In</button>
+      <button class="btn btn-primary" @click="signUp">Sign Up</button>
     </div>
     <br><br>
     <p>{{error.message}}</p>
     <br>
-    <router-link to="/signup">Not a user? Sign up</router-link>
+    <router-link to="/signin">Already a user? Sign in</router-link>
   </div>
 </template>
 
@@ -25,10 +25,11 @@ import { firebaseApp } from '../firebaseApp'
         }
       }
     },
+
     methods: {
-      signIn() {
-        firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
-          .catch(error => {
+      signUp() {
+        firebaseApp.auth().createUserWithEmailAndPassword(this.email, this.password)
+          .cath(error => {
             this.error = error
           })
       }
